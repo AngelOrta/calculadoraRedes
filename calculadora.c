e5f03a4f5f9309c91f1114cbee19d5a64b14e8ab
AngelOrta <146902560+AngelOrta@users.noreply.github.com>
2024-05-25 02:13:34 -0600

    Agregar menu en bucle

--- calculadora.c
+++ calculadora.c
@@ -8,14 +8,19 @@
 void interfaz(char *direccion, int *mascara, int *mascaraOpci);
 void limpiaBufferIn();
 int main(){
+    char opcion;
 	char direccion[17]; //Direccion de un host o red
-    int mascara,mascaraOpci;
+    int mascara,mascaraOpci,i=0;
     //Empieza Calculadora
-
-    interfaz(direccion,&mascara,&mascaraOpci);
-    printf("\n%.15s %d %d",direccion,mascara,mascaraOpci);
-    fflush(stdout);
-    sleep(8);
+    do{
+        if(i!=0)
+            limpiaBufferIn();
+        interfaz(direccion,&mascara,&mascaraOpci);
+        i++;
+        printf("\n%.15s %d %d",direccion,mascara,mascaraOpci);
+        fflush(stdout);
+        printf("\nQuieres probar con otra dirección IP? (S/N): ");
+    }while((opcion=getchar()) == 115 || opcion == 83);
     return 0;
 }
 
@@ -24,6 +29,9 @@ int main(){
 //Imprime la interfaz y captura los datos desde el teclado
 void interfaz(char *direccion, int *mascara, int *mascaraOpci){
     char opci[4];
+    printf("\033[H\033[2J");
+    fflush(stdout);
+    
     printf("================================================================================\n\n");
     printf("\t\t\t    CALCULADORA DE REDES\n\n");
     printf("________________________________________________________________________________\n");
@@ -34,12 +42,13 @@ void interfaz(char *direccion, int *mascara, int *mascaraOpci){
     printf("                                /                     |                        |\n");
     printf("________________________________|_____________________|________________________|\n");
     printf("\033[2A\033[7C"); //Cursor a primer slot
-    
+
     if(fgets(direccion, 17, stdin)){
         if(direccion[strlen(direccion)-1] != '\n') //Excedio numero caracteres permitidos
             limpiaBufferIn();
         direccion[strlen(direccion)-1] = 0; //Si habia salto de linea
     }
+    
     printf("\033[1B\033[20D"); //Cursor a salida
     printf("\033[2A\033[42C"); //Cursor a segundo slot
     
