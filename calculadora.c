181bf89aaf3fb979d65d9ecbe1779f91edd6ed68
AngelOrta <146902560+AngelOrta@users.noreply.github.com>
2024-05-25 01:02:50 -0600

    Correccion de desbordamiento de bufer al ingresar datos

--- calculadora.c
+++ calculadora.c
@@ -6,10 +6,12 @@
 #include<string.h>
 #include<unistd.h>
 void interfaz(char *direccion, int *mascara, int *mascaraOpci);
+void limpiaBufferIn();
 int main(){
-	char direccion[17];
+	char direccion[17]; //Direccion de un host o red
     int mascara,mascaraOpci;
     //Empieza Calculadora
+
     interfaz(direccion,&mascara,&mascaraOpci);
     printf("\n%.15s %d %d",direccion,mascara,mascaraOpci);
     fflush(stdout);
@@ -17,33 +19,49 @@ int main(){
     return 0;
 }
 
+
+
 //Imprime la interfaz y captura los datos desde el teclado
 void interfaz(char *direccion, int *mascara, int *mascaraOpci){
+    char opci[4];
     printf("================================================================================\n\n");
     printf("\t\t\t    CALCULADORA DE REDES\n\n");
     printf("________________________________________________________________________________\n");
-    printf("\t\t\t\t|  \t\t      |  Máscara de subred\t|\n");//  Mascara de subred  
-    //printf("  Mascara de subred  |");
-    //printf("  Mascara de subred\t|\n");
+    printf("\t\t\t\t|  \t\t      |  Máscara de subred\t|\n"); 
     printf("\tDirección IP\t\t|  Máscara de subred  |  para sub/superneteo\t|\n");
     printf("\t\t\t\t|  (24, por ejemplo)  |  (opcional)\t\t|\n");
     printf("________________________________|_____________________|________________________|\n");
-    printf("                                |                     |                        |\n");
+    printf("                                /                     |                        |\n");
     printf("________________________________|_____________________|________________________|\n");
     printf("\033[2A\033[7C"); //Cursor a primer slot
-    fflush(stdout);
-    fgets(direccion, 17, stdin);
-    fflush(stdin);
+    
+    if(fgets(direccion, 17, stdin)){
+        if(direccion[strlen(direccion)-1] != '\n') //Excedio numero caracteres permitidos
+            limpiaBufferIn();
+        direccion[strlen(direccion)-1] = 0; //Si habia salto de linea
+    }
     printf("\033[1B\033[20D"); //Cursor a salida
     printf("\033[2A\033[42C"); //Cursor a segundo slot
-    fflush(stdout);
+    
     scanf("%d",&(*mascara));
-    fflush(stdin);
+    limpiaBufferIn();
     printf("\033[1B\033[22D"); //Cursor a salida
     printf("\033[2A\033[60C"); //Cursor a tercer slot
-    fflush(stdout);
-    scanf("%d",&(*mascaraOpci));
-    fflush(stdin);
+    
+    if(fgets(opci,sizeof(opci),stdin)){
+        if(opci[0] == '\n') //No se introdujo mascara opcional
+            *mascaraOpci = -1; 
+        else{
+            if(opci[strlen(opci)-1] != '\n')
+                limpiaBufferIn();
+            opci[strlen(opci)-1] = 0;
+            *mascaraOpci = atoi(opci);
+        }
+    }
     printf("\033[0B\033[22D"); //Cursor a salida
-    fflush(stdout);
+}
+
+void limpiaBufferIn(){
+    int c;
+    while((c=getchar()) != '\n' && c != EOF);
 }
